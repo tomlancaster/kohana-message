477851f264341093a23e0424b169a1f3cf612fef
Ian Bari <ian.bari@att.net>
2014-12-14 00:44:30 -0800

    Update to Bootstrap 3.3.1

--- views/message/bootstrap.php
+++ views/message/bootstrap.php
@@ -1,28 +1,40 @@
 <div class="row">
-	<?php if (Arr::get($messages, 'error')) :?>
-	<div class="span12">
-		<div class="alert alert-danger">
+	<?php if (Arr::get($messages, 'success')) : ?>
+	<div class="col-lg-12">
+		<div class="alert alert-success" role="alert">
 			<a class="close" data-dismiss="alert" href="#">&times;</a>
-			<strong><?php echo __('Error') ?></strong>
-			<?php echo implode('<br/>', $messages['error'])?>
+			<strong><?php echo __('Success!') ?>&nbsp;</strong>
+			<?php echo join('<br>', $messages['success']) ?>
+		</div>
+	</div>
+	<?php endif; ?>
+	
+	<?php if (Arr::get($messages, 'error')) : ?>
+	<div class="col-lg-12">
+		<div class="alert alert-danger" role="alert">
+			<a class="close" data-dismiss="alert" href="#">&times;</a>
+			<strong><?php echo __('Woops!') ?>&nbsp;</strong>
+			<?php echo join('<br>', $messages['error']) ?>
 		</div>
 	</div>
 	<?php endif;
-	if (Arr::get($messages, 'success')) :?>
-	<div class="span12">
-		<div class="alert alert-success">
+	
+	if (Arr::get($messages, 'warning')) : ?>
+	<div class="col-lg-12">
+		<div class="alert alert-warning" role="alert">
 			<a class="close" data-dismiss="alert" href="#">&times;</a>
-			<strong><?php echo __('Success') ?></strong>
-			<?php echo implode('<br/>', $messages['success'])?>
+			<strong><?php echo __('Warning!') ?>&nbsp;</strong>
+			<?php echo join('<br>', $messages['warning']) ?>
 		</div>
 	</div>
-	<?php endif;	
-	if (Arr::get($messages, 'notice')) :?>	
-	<div class="span12">
-		<div class="alert alert-info">
+	<?php endif;
+
+	if (Arr::get($messages, 'notice')) : ?>	
+	<div class="col-lg-12">
+		<div class="alert alert-info" role="alert">
 			<a class="close" data-dismiss="alert" href="#">&times;</a>
-			<strong><?php echo __('Information') ?></strong>
-			<?php echo implode('<br/>', $messages['notice'])?>
+			<strong><?php echo __('Heads up!') ?>&nbsp;</strong>
+			<?php echo join('<br>', $messages['notice']) ?>
 		</div>
 	</div>
 	<?php endif; ?>
